03c110efb262bc03122c1b2f76a5137eb4c3cdca
nikhila-123 <thagirisha.nikhila@gmail.com>
2022-04-26 15:51:47 +0530

    commit on master

---
+++ index.js
@@ -0,0 +1,66 @@
+let tableData1=""; 
+let tableheader="";
+function load(){  
+let title=document.getElementById("title") 
+title.textContent="Storing data"     
+fetch("https://jsonplaceholder.typicode.com/posts").then((data)=>{
+return data.json();  
+}).then((objectData)=>{
+console.log(objectData[0].title);
+let tableData="";
+tableheader+= `
+<tr>
+<th>userId</th>
+<th onclick="sort()">id</th>
+<th>title</th>
+<th>body</th>
+</tr>`
+document.getElementById("text").innerHTML=tableheader;
+objectData.map((values)=>{
+ tableData+= `<tr>
+ <td><a href="#" onclick="test(${values.id})">${values.userId}</a></td>
+ <td>${values.id}</td>
+<td>${values.title}</td>
+<td>${values.body}</td>
+<tr>`
+tableData1=tableData;
+//document.getElementById("table_body").innerHTML=tableData;
+});
+document.getElementById("remove").innerHTML="";
+document.getElementById("text").innerHTML=tableheader;
+document.getElementById("table_body").innerHTML=tableData;
+
+});}
+
+function test(k){
+console.log(k);
+fetch("https://jsonplaceholder.typicode.com/posts/"+k+"").then((data)=>{
+return data.json(); 
+}).then((objectData)=>{
+console.log(objectData);
+let tableData="";
+tableData+= `
+<tr>
+
+ <td>${objectData.userId}</td>
+ <td>${objectData.id}</td>
+<td>${objectData.title}</td>
+<td>${objectData.body}</td>
+<tr>`
+document.getElementById("table_body").innerHTML=tableData;
+});
+let back1 = document.getElementById("dummy");
+let btn = `<h1  onclick="back()">back</h1>`
+back1.innerHTML=btn;
+}
+function back(){
+console.log("test")
+document.getElementById("table_body").innerHTML=tableData1;
+let back1 = document.getElementById("dummy")
+back1.innerHTML="";
+}
+
+function sort(){
+    let p="nikhi"
+    console.log(p)
+}
